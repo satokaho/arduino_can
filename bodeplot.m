2f25e8c953e06e9b15eaf6b5e01ab5d17fde00db
sato kaho <clf13078@ict.nitech.ac.jp>
2020-06-26 15:37:18 +0900

    Update bodeplot.m

--- bodeplot.m
+++ bodeplot.m
@@ -7,15 +7,17 @@ set(0, 'defaultTextFontSize', 16);
 set(0, 'defaultTextFontName', 'times');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%rooptime=35,kd=100,kp=100,f=0.25~32
 % numset = [ 11 12 13 14 15 16 17 ...
 %             21 22 23 24 25 26 27 28 ...
 %             31 32 33 34 35 36 37 38];
 % freqset = [0.25 0.5 1 2 4 8 16 ...
 %             0.25 0.5 1 2 4 8 16 32 ...
 %             0.25 0.5 1 2 4 8 16 32];
-numset = [ 11 12 13 14 15 16 17];
-freqset = [0.25 0.5 1 2 4 8 16];
+numset = [11 12 13 14 15 16 17];
+freqset = [0.25 0.5 1 2 4 8 16 ];
 figure        
+gain = [];
 
 for i_n = 1:length(numset)
     filename = ['freqRespData',num2str(numset(i_n)),'.mat'];
@@ -28,9 +30,22 @@ for i_n = 1:length(numset)
         data(i_n).outputout(i_t,1) = Dataset(i_t,5);
     end
     
+    
     inputAmp = max(data(i_n).inputout);
     outputAmp = max(data(i_n).outputout);
     
-    loglog(data(i_n).freq, outputAmp/inputAmp, 'o')
-    hold on
+   
+    gain = [gain;outputAmp/inputAmp];
+    
+    
 end
+
+    loglog([data(:).freq], gain, '-')
+    %loglog(data(i_n).freq, outputAmp/inputAmp, 'o')
+    
+    xlabel('frequency [rps]', 'interpreter', 'latex', 'Fontsize', 14);
+    ylabel('magnitude[dB]', 'interpreter', 'latex', 'Fontsize', 14);
+    xlim([0.25,max(data(i_n).freq)]);
+    
+    
+    hold on
